ceed4a57b1c8271c8531dfda63946d856177a347
pedropaes <pedropaes@gmail.com>
2019-06-03 16:48:43 +0100

    sdjbjh

---
+++ Script/Povoamento.sql
@@ -0,0 +1,133 @@
+﻿
+DELETE FROM Eloise.dbo.Alergenio;
+DELETE FROM Eloise.dbo.IngredienteAlergenio;
+DELETE FROM Eloise.dbo.IngredienteReceita;
+DELETE FROM Eloise.dbo.Receita;
+DELETE FROM Eloise.dbo.Dificuldade;
+DELETE FROM Eloise.dbo.Ingrediente;
+DELETE FROM Eloise.dbo.Passo;
+DELETE FROM Eloise.dbo.Regime;
+DELETE FROM Eloise.dbo.Tipo;
+DELETE FROM Eloise.dbo.ValorNutricional;
+DELETE FROM Eloise.dbo.[User];
+
+--Povoamento Alergenio
+BULK INSERT Eloise.dbo.Alergenio FROM 'C:\Users\Pedro\Desktop\Script\Alergenio.csv'
+	WITH (
+		ROWTERMINATOR = '\n',
+		FIELDTERMINATOR = ';',
+		CODEPAGE = 'ACP',
+		KEEPIDENTITY
+	);
+
+SELECT * FROM Eloise.dbo.Alergenio;
+--Povoamento Dificuldade
+
+BULK INSERT Eloise.dbo.Dificuldade FROM 'C:\Users\Pedro\Desktop\Script\Dificuldade.csv'
+	WITH (
+		ROWTERMINATOR = '\n',
+		FIELDTERMINATOR = ';',
+		CODEPAGE = 'ACP',
+		KEEPIDENTITY
+	);
+
+SELECT * FROM Eloise.dbo.Dificuldade;
+
+--Povoamento Ingredientes
+BULK INSERT Eloise.dbo.Ingrediente FROM 'C:\Users\Pedro\Desktop\Script\Ingredientes.csv'
+	WITH (
+		ROWTERMINATOR = '\n',
+		FIELDTERMINATOR = ';',
+		CODEPAGE = 'ACP',
+		KEEPIDENTITY
+	);
+
+SELECT * FROM Eloise.dbo.Ingrediente;
+
+--Povoamento IngredientesAlergenio
+BULK INSERT Eloise.dbo.[IngredienteAlergenio] FROM 'C:\Users\Pedro\Desktop\Script\IngredientesAlergenio.csv'
+	WITH (
+		ROWTERMINATOR = '\n',
+		FIELDTERMINATOR = ';',
+		CODEPAGE = 'ACP',
+		KEEPIDENTITY
+	);
+
+SELECT * FROM Eloise.dbo.[IngredienteAlergenio];
+
+--Povoamento IngredientesReceita
+BULK INSERT Eloise.dbo.IngredienteReceita FROM 'C:\Users\Pedro\Desktop\Script\IngredientesReceita.csv'
+	WITH (
+		ROWTERMINATOR = '\n',
+		FIELDTERMINATOR = ';',
+		CODEPAGE = 'ACP',
+		KEEPIDENTITY
+	);
+
+SELECT * FROM Eloise.dbo.IngredienteReceita;
+
+--Povoamento Passo
+BULK INSERT Eloise.dbo.Passo FROM 'C:\Users\Pedro\Desktop\Script\Passos.csv'
+	WITH (
+		ROWTERMINATOR = '\n',
+		FIELDTERMINATOR = ';',
+		CODEPAGE = 'ACP',
+		KEEPIDENTITY
+	);
+
+SELECT * FROM Eloise.dbo.Passo;
+
+--Povoamento Receita
+BULK INSERT Eloise.dbo.Receita FROM 'C:\Users\Pedro\Desktop\Script\Receita.csv'
+	WITH (
+		ROWTERMINATOR = '\n',
+		FIELDTERMINATOR = ';',
+		CODEPAGE = 'ACP',
+		KEEPIDENTITY
+	);
+
+SELECT * FROM Eloise.dbo.Receita;
+
+--Povoamento Regime
+BULK INSERT Eloise.dbo.Regime FROM 'C:\Users\Pedro\Desktop\Script\Regime.csv'
+	WITH (
+		ROWTERMINATOR = '\n',
+		FIELDTERMINATOR = ';',
+		CODEPAGE = 'ACP',
+		KEEPIDENTITY
+	);
+
+SELECT * FROM Eloise.dbo.Regime;
+
+--Povoamento Tipo
+BULK INSERT Eloise.dbo.Tipo FROM 'C:\Users\Pedro\Desktop\Script\Tipo.csv'
+	WITH (
+		ROWTERMINATOR = '\n',
+		FIELDTERMINATOR = ';',
+		CODEPAGE = 'ACP',
+		KEEPIDENTITY
+	);
+
+SELECT * FROM Eloise.dbo.Tipo;
+
+--Povoamento ValorNutricional
+BULK INSERT Eloise.dbo.ValorNutricional FROM 'C:\Users\Pedro\Desktop\Script\ValorNutricional.csv'
+	WITH (
+		ROWTERMINATOR = '\n',
+		FIELDTERMINATOR = ';',
+		CODEPAGE = 'ACP',
+		KEEPIDENTITY
+	);
+
+SELECT * FROM Eloise.dbo.ValorNutricional;
+
+--Povoamento User
+BULK INSERT Eloise.dbo.[User] FROM 'C:\Users\Pedro\Desktop\Script\User.csv'
+	WITH (
+		ROWTERMINATOR = '\n',
+		FIELDTERMINATOR = ';',
+		CODEPAGE = 'ACP',
+		KEEPIDENTITY
+	);
+
+SELECT * FROM Eloise.dbo.[User];
---
+++ wwwroot/js/eloise.js
@@ -0,0 +1,113 @@
+function speak(input) {
+    responsiveVoice.speak(input, "Portuguese Female");
+}
+
+function wait(ms) {
+    var d = new Date();
+    var d2 = null;
+    do { d2 = new Date(); }
+    while (d2 - d < ms);
+}
+window.onload = function () {
+    var numero = 1;
+    var audio = new Audio('/sounds/beep.mp3');
+    alert('passos:' + unescape(numero));
+    var arr = ["Antes de iniciar a receita, certifique-se que tem todos os ingredientes.  Quando estiver pronto para iniciar a receita, diga começar."];
+    var actual = 0;
+    var proximo;
+    annyang.setLanguage('pt-PT');
+    if (annyang) {
+        // Let's define a command.
+
+        console.log("It detected annyang");
+
+        var commands = {
+            'procura *tag': procura,
+            'procurar *tag': procura,
+            'preparar receita': preparar,
+            'ver receita': ver,
+            'tutorial': tutorial,
+            'começar': comecar,
+            'próximo': next,
+            'seguinte': next,
+            'anterior': previous,
+            'volta': previous,
+            'passo *num': passo,
+            'sair': sair,
+        };
+
+        function procura(tag) {
+            //audio.play();
+            //speak('A procurar ' + tag);
+            //wait(1000);
+            document.getElementById('procura').setAttribute('value', tag);
+            document.getElementById('procurar').click();
+        };
+
+        function comecar() {
+            if (actual == 0) {
+                speak(arr[actual]);
+                actual += 1;
+                document.getElementById('next').click();
+                speak(arr[actual]);
+            }
+        }
+
+        function next() {
+            proximo = -1;
+            if (actual + proximo < 1) {
+                speak('não existe passo anterior');
+            }
+            else {
+                actual = actual + proximo;
+                document.getElementById('previous').click();
+                speak(arr[actual]);
+            }
+        }
+
+        function previous() {
+            proximo = -1;
+            if (actual + proximo < 1) {
+                speak('não existe passo anterior');
+            }
+            else {
+                actual = actual + proximo;
+                document.getElementById('previous').click();
+                speak(arr[actual]);
+            }
+        }
+        function preparar() {
+            document.getElementById('preparar').click();
+        }
+
+        function ver() {
+            document.getElementById('ver').click();
+        }
+        function tutorial() {
+            document.getElementById('tutorial').click();
+        }
+
+        function passo(num) {
+            $(document).ready(function () {
+                $('#carouselExampleIndicators').carousel(parseInt(num));
+            });
+            if (parseInt(num) <= numero + 1) {
+                actual = parseInt(command);
+                speak(arr[actual]);
+            }
+            else speak('Passo não existe!!!!');
+        }
+
+        function sair() {
+            speak('Sair');
+            wait(1000);
+            document.getElementById('inicio').click();
+        }
+        // Add our commands to annyang
+        annyang.addCommands(commands);
+
+        // Start listening.
+        annyang.start();
+    }
+
+}
